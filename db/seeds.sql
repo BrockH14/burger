bc520328acc841fb39e6be653d857b5e2651fb88
BrockH <brock.horton@gmail.com>
2020-05-06 15:53:59 -0400

    schema and seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom swiss Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Jalapeno Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Plain Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double cheese Burger', true);
