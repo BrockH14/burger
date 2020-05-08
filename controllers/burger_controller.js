c54292f19e4ed1dc5b79b279bf3929e5e4460463
BrockH <brock.horton@gmail.com>
2020-05-08 10:53:07 -0400

    last push

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -2,9 +2,8 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -12,30 +11,26 @@ router.get("/", function(req, res) {
     res.render("index", hbsObject);
   });
 });
-
 router.post("/api/burgers", function(req, res) {
-  burger.create([
+  burger.insertOne([
     "name", "devoured"
   ], [
     req.body.name, req.body.devoured
   ], function(result) {
-    // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
   console.log("condition", condition);
-  burger.update({
+  burger.updateOne({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
     }
   });
 });
-// Export routes for server.js to use.
 module.exports = router;
