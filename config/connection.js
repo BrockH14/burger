04b3db5f1424573cfd98db27b2cf06520d4c6664
BrockH <brock.horton@gmail.com>
2020-05-08 12:47:42 -0400

    heroku error fix 010

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,12 @@
 var mysql = require("mysql");
-var connection;
-
-if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
-    connection = mysql.createConnection({
-        host: "127.0.0.1",
-        port: 3306,    
-        user: "root",
-        password: "Kaydoe1414",
-        database: "burger_db"
-    });
-}
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Kaydoe1414",
+  database: "burger_db",
+  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+});
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
